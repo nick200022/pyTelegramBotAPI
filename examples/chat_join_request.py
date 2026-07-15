e8cce63d9e05e0fc22826fc972b4595566fab768
PEDRO NICOLAS CAMACHO PERILLA <nicolas20002sa@gmail.com>
2026-07-15 12:30:15 -0500

    Implement chat type handling in chat_join_request
    Added handling for different chat types in chat_join_request.

--- examples/chat_join_request.py
+++ examples/chat_join_request.py
@@ -9,3 +9,14 @@ def make_some(message: telebot.types.ChatJoinRequest):
     bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
 bot.infinity_polling(allowed_updates=telebot.util.update_types)
+if message.chat.type == "private":
+    # private chat message
+
+if message.chat.type == "group":
+	# group chat message
+
+if message.chat.type == "supergroup":
+	# supergroup chat message
+
+if message.chat.type == "channel":
+	# channel message
